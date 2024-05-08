6bc423067e0f48395ebf490dbc863630a80835b5
Shirakiinn <124741301+Shirakiinn@users.noreply.github.com>
2024-05-07 22:17:51 -0300

    tradu

--- components/info.tsx
+++ components/info.tsx
@@ -29,13 +29,13 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       <hr className="my-4" />
       <div className="flex flex-col gap-y-6">
         <div className="flex items-center gap-x-4">
-          <h3 className="font-semibold text-black">Size:</h3>
+          <h3 className="font-semibold text-black">Tamanho:</h3>
           <div>
             {data?.size?.value}
           </div>
         </div>
         <div className="flex items-center gap-x-4">
-          <h3 className="font-semibold text-black">Color:</h3>
+          <h3 className="font-semibold text-black">Cor:</h3>
           <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
         </div>
       </div>
