ca43f01a699bd691123fc152d77eeae84c705b5a
Shirakiinn <124741301+Shirakiinn@users.noreply.github.com>
2024-06-07 22:09:50 -0300

    sss

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -1,14 +1,24 @@
-import { Billboard } from "@/types";
+import { Billboard as BillboardType } from "@/types";
 import React from 'react';
 
 interface BillboardProps {
-  data: Billboard | null; // Explicitly allow data to be null
+  data: BillboardType | null; // Explicitly allow data to be null
 }
 
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
-  // Check if data is null or undefined before rendering
-  if (!data) {
-    return <div>Loading...</div>; // Or any other placeholder content
+
+  if (!data || !data.imageUrl) {
+    return (
+      <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+        <div className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-gray-200">
+          <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
+            <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+              Default Label
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -25,3 +35,4 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
 };
 
 export default Billboard;
+  
