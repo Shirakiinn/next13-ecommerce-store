41fc5dbe78904fb931f7a142cac101606c696a78
arthmont <124741301+Shirakiinn@users.noreply.github.com>
2024-06-11 20:50:59 -0300

    banners e texto dos banners

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -12,7 +12,7 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
       <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
         <div className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-gray-200">
           <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-            <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+            <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs text-white shadow-text">
               Default Label
             </div>
           </div>
@@ -25,7 +25,7 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div style={{ backgroundImage: `url(${data.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+          <div className="font-bold text-4xl sm:text-5xl lg:text-8xl sm:max-w-xl max-w-xs text-white shadow-text">
             {data.label || 'Default Label'} {/* Provide a default value */}
           </div>
         </div>
@@ -35,4 +35,3 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
 };
 
 export default Billboard;
-  
