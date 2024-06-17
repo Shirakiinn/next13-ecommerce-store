bc4b104af176b96f3fe4e7fad1d00c9fbbf0e6f9
arthmont <124741301+Shirakiinn@users.noreply.github.com>
2024-06-17 14:54:36 -0300

    navbar finalizada

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Link from "next/link";
 import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
@@ -11,24 +9,19 @@ import TopBar from "@/components/topbar"; // Import the TopBar component
 const Navbar = async () => {
   const categories = await getCategories();
 
-  return (
-    <div>
+  return ( 
+    <div className="border-b">
       <TopBar /> {/* Add the TopBar component here */}
-      <div className="sticky-navbar">
-        <Container>
-          <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-            <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2 items-center">
-              <img src="https://tinypic.host/images/2024/06/11/OIG1.2NXAfdCmhiE-removebg.png" alt="Logo" style={{ height: 'auto', width: '100px' }} />
-            </Link>
-            <MainNav data={categories} />
-            <NavbarActions />
-          </div>
-        </Container>
-      </div>
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2 items-center">
+            <img src="https://tinypic.host/images/2024/06/11/OIG1.2NXAfdCmhiE-removebg.png" alt="Logo" style={{ height: 'auto', width: '100px' }} />
+          </Link>
+          <MainNav data={categories} />
+          <NavbarActions />
+        </div>
+      </Container>
       <InfoBar /> {/* Add the InfoBar component here */}
-      <div style={{ paddingTop: '64px' }}>
-        {/* Your page content goes here */}
-      </div>
     </div>
   );
 };
