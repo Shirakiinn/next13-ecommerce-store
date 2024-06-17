68c9def091ca9e7f9b2fc54b7ae805b979ec4d23
arthmont <124741301+Shirakiinn@users.noreply.github.com>
2024-06-17 14:50:05 -0300

    efeito debaixo da letra

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -22,23 +22,24 @@ const MainNav: React.FC<MainNavProps> = ({
   }));
 
   return (
-    <nav
-      className="mx-6 flex items-center space-x-4 lg:space-x-6"
-    >
+    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
       {routes.map((route) => (
         <Link
           key={route.href}
           href={route.href}
           className={cn(
-            'text-sm font-medium transition-colors hover:text-black',
-            route.active ? 'text-black' : 'text-neutral-500'
+            'text-lg font-bold transition-colors hover:text-black relative', // Texto grande e em negrito
+            route.active ? 'text-red-500' : 'text-neutral-500' // Cor do texto quando ativo
           )}
         >
           {route.label}
-      </Link>
+          {route.active && (
+            <span className="absolute left-0 right-0 -bottom-2 h-0.5 bg-red-500"></span> // Linha inferior colorida quando ativo
+          )}
+        </Link>
       ))}
     </nav>
-  )
+  );
 };
 
 export default MainNav;
